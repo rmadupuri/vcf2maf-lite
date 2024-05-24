a2e7424bff1e608b090d9aa3c8badd633dd9d164
Ramya Madupuri <ramyamadupuri99@gmail.com>
2024-05-24 10:52:42 -0400

    fix

--- tests/vcf2maf_lite_tests.py
+++ tests/vcf2maf_lite_tests.py
@@ -3,8 +3,6 @@
 import unittest
 import tempfile
 import os
-import sys
-sys.path.append('..')
 from vcf2maf_lite import extract_vcf_data_from_file
 from vcf2maf_lite import write_standardized_mutation_file
 
